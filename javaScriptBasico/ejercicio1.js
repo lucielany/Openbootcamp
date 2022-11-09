7615a6ca3e560ac7d2445280c439902aff3ae9fb
Lucielany <42656671+lucielany@users.noreply.github.com>
2022-11-09 23:38:55 +0100

    Update ejercicio1.js

--- javaScriptBasico/ejercicio1.js
+++ javaScriptBasico/ejercicio1.js
@@ -1,13 +1,8 @@
-const persona = {
-  nombre: "Luciela",
-  edad: 36,
-  desarrollador: true,
-  fechaNacimiento: new Date(1986, 9, 31),
-  libroFavorito: {
-    titulo: "El progreso del peregrino",
-    autor: "John Bunyan",
-    fecha: 1678,
-    url: "https://books.google.es/books/about/El_progreso_del_peregrino_para_todos.html?id=jDihDwAAQBAJ&printsec=frontcover&source=kp_read_button&hl=es&redir_esc=y#v=onepage&q&f=false"
-  }
-}
-console.log(persona);
+const lista = ["Luciela", 34, true,new Date(1986, 09, 31),
+    { 
+        titulo: "El progreso del peregrino",
+        autor: "John Bunyan",
+        fecha: 1678,
+        url: "https://books.google.es/books/about/El_progreso_del_peregrino_para_todos.html?id=jDihDwAAQBAJ&printsec=frontcover&source=kp_read_button&hl=es&redir_esc=y#v=onepage&q&f=false"
+      },
+];
