60eb58f40d86c1ef3af73781e53c3b7d389956c2
Lucielany <42656671+lucielany@users.noreply.github.com>
2022-11-09 23:40:39 +0100

    Delete ejercicio1.js

--- javaScriptBasico/ejercicio1.js
+++
@@ -1,8 +0,0 @@
-const lista = ["Luciela", 36, true,new Date(1986, 09, 31),
-    { 
-        titulo: "El progreso del peregrino",
-        autor: "John Bunyan",
-        fecha: 1678,
-        url: "https://books.google.es/books/about/El_progreso_del_peregrino_para_todos.html?id=jDihDwAAQBAJ&printsec=frontcover&source=kp_read_button&hl=es&redir_esc=y#v=onepage&q&f=false"
-      },
-];
