a587256314a3f4eca7c22c16dd62f687c5b37e15
Lucielany <42656671+lucielany@users.noreply.github.com>
2023-03-14 12:26:55 +0100

    Update index.js

--- JavaScriptBasico/10-modulos/index.js
+++ JavaScriptBasico/10-modulos/index.js
@@ -17,3 +17,27 @@ const sum = suma(12, 90)
 console.log(sum)
 
 // console.log(module)
+
+/*function suma(a, b) {
+  return a + b
+}
+
+function multiplicar(a, b) {
+  return a * b
+}
+
+function eleva(a, b) {
+  return a ** b
+}
+
+function factorial(a) {
+  //factorial de 5: 5 * 4 * 3 * 2 * 1
+  for(let i = 1; i <= a; i++) {
+    factorial *= i;
+  }
+  return factorial;
+}
+
+const fact = factorial(5)
+console.log(fact)
+*/
